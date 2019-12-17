13d7474919fe51dce5da1b250a3d41320da6d3d7
Balint Ligeti <balint.ligeti@hotmail.com>
2019-12-17 11:53:58 +0100

    final version

--- guessinggame_gyakorlo.py
+++ guessinggame_gyakorlo.py
@@ -1,26 +1,26 @@
 import math
 import random
 
-a = [random.randint(1,99) for x in range(10)]
-b = [random.randint(1,49) for x in range(10)]
+one_to_99 = [random.randint(1,99) for x in range(10)]
+one_to_49 = [random.randint(1,49) for x in range(10)]
+
+def guess_a_num(to):
+   guess = int(input(f"Enter an integer from 1 to {to}: "))
+   return guess
 
 def check_good_number(list, to):
-    if to == 1:
-        max = 99
-    else:
-        max = 49
     for i in range(10):
-        guessed_number = int(input(f"Enter an integer from 1 to {max}: "))
+        (guessed_number) = guess_a_num(to)
         while list[i] != guessed_number:
             if (guessed_number) < list[i]:
                 print("guess is low")
-                guessed_number = int(input(f"Enter an integer from 1 to {max}: "))
+                guessed_number = guess_a_num(to)
             elif guessed_number > list[i]:
                 print("guess is high")
-                guessed_number = int(input(f"Enter an integer from 1 to {max}: "))
+                guessed_number = guess_a_num(to)
             else:
                 break
         print("you guessed it!")
 
-check_good_number(a, 1)
-check_good_number(b, 2)
+check_good_number(one_to_99,99)
+check_good_number(one_to_49,49)
